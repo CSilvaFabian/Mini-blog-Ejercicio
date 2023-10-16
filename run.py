641b9990ea6105c0ab2527ca9a0fd795aa65d19a
CSilvaFabian <cruzfabian292@gmail.com>
2023-10-16 17:21:31 -0600

    trabajando con formularios

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 
 app = Flask(__name__)
 
-posts = [321665, 65654, 544, 4655]
+posts = [321665, 65654, 544, 4655, 212 ,1212, 2121]
 
 @app.route("/")
 def index():
@@ -19,3 +19,20 @@ def show_post(slug):
 @app.route("/admin/post/<int:post_id>/")
 def post_form(post_id=None):
     return render_template("admin/post_form.html", post_id=post_id)
+
+@app.route("/signup/", methods=["GET", "POST"])
+def show_signup_form():
+    return render_template("signup_form.html")
+
+@app.route("/signup/", methods=["GET", "POST"])
+def show_signup_form():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+
+        next = request.args.get('next', None)
+        if next:
+            return redirect(next)
+        return redirect(url_for('index'))
+    return render_template("signup_form.html")
